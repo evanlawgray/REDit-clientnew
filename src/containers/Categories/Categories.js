fcaa9663aac29fb3971e917e1853309a3ce1d1a1
Evan Gray <evanlawgray@gmail.com>
2017-05-04 22:33:52 -0700

    Update Dategories.js.

--- src/containers/Categories/Categories.js
+++ src/containers/Categories/Categories.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './styles.css';
+
 import Week from '../../components/Week';
 
-const Categories = ({ weeks }) => {
+const Categories = ({ weeks, getPosts }) => {
   return (
-    <div>
-      {weeks.map(week => (
-        <Week
-          key={week.id}
-          title={week.title}
-          categories={week.categories}
-        />
-      ))}
-    </div>
+    <ul className={styles.weeksList}>
+      {
+        weeks.length < 0 &&
+        <h3>Loading Weeks...</h3>
+      }
+      {
+        weeks.length > 0 &&
+        weeks.map(week => (
+          <Week
+            key={week.week + Date.now()}
+            title={week.week}
+            lessons={week.lessons}
+            getPosts={getPosts}
+          />
+        ))
+      }
+    </ul>
   );
 };
 
 Categories.propTypes = {
-  weeks: PropTypes.obj
+  weeks: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Categories;
+
